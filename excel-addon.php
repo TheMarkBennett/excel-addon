5969da3563703a8436976eaffb89e589ab81c71a
Mark Bennett <mark.bennett@ucf.edu>
2019-03-14 10:05:26 -0400

    int

---
+++ excel-addon.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Excel addons
+ * Plugin URI: https://excel.ucf.edu
+ * Description: Adds features to the excel theme
+ * Version: 1.0
+ * Author: Mark Bennett
+ * Author URI: https://excel.ucf.edu
+ */
